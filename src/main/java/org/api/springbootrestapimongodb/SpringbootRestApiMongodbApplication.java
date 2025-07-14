37cd269b156c2adb1ef940a3408fec3fcb9693b8
Sergio Henrique <sergimmatos@hotmail.com>
2025-07-14 18:12:18 -0300

    feat: Initial prject structure

---
+++ src/main/java/org/api/springbootrestapimongodb/SpringbootRestApiMongodbApplication.java
@@ -0,0 +1,13 @@
+package org.api.springbootrestapimongodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootRestApiMongodbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootRestApiMongodbApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/api/springbootrestapimongodb/SpringbootRestApiMongodbApplicationTests.java
@@ -0,0 +1,13 @@
+package org.api.springbootrestapimongodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootRestApiMongodbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
