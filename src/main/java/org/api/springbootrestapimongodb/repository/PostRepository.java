4b088c40b31207e0937bb102c662e2b4850cf9d7
Sergio Henrique <sergimmatos@hotmail.com>
2025-07-16 17:08:54 -0300

    Searching with @Query

--- src/main/java/org/api/springbootrestapimongodb/repository/PostRepository.java
+++ src/main/java/org/api/springbootrestapimongodb/repository/PostRepository.java
@@ -2,12 +2,16 @@ package org.api.springbootrestapimongodb.repository;
 
 import org.api.springbootrestapimongodb.domain.Post;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
 public interface PostRepository extends MongoRepository<Post, String> {
+
+    @Query("{ 'title': { $regex: ?0, $options: 'i' } }")
+    List<Post> searchTitle(String title);
 
     List<Post> findByTitleContainingIgnoreCase(String title);
 }
--- src/main/java/org/api/springbootrestapimongodb/service/PostService.java
+++ src/main/java/org/api/springbootrestapimongodb/service/PostService.java
@@ -28,4 +28,6 @@ public class PostService {
     public List<Post> findByTittle(String title) {
         return postRepository.findByTitleContainingIgnoreCase(title);
     }
+
+
 }
